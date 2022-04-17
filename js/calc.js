7f4463e780a9e7400841f31216b7f1915b41a8f0
EvansMutwiri <mutwirievansm@gmail.com>
2022-04-17 14:06:17 +0300

    js project sass styling

--- js/calc.js
+++ js/calc.js
@@ -1,15 +1,23 @@
 const button = document.querySelector('#btn');
-button.addEventListener('click', () => calc());
+button.addEventListener('click', function() {
+    calc();
+    this.form.reset();
+});
 
-function calc () {
-    let a = parseInt(document.querySelector('#value1').value || 0);
-    let b = parseInt(document.querySelector('#value2').value || 0);
+function calc() {
+    let a = parseInt(document.querySelector('#value1').value);
+    let b = parseInt(document.querySelector('#value2').value);
+
+    if (isNaN(a) || isNaN(b)) {
+        alert("Please enter only number values: ");
+        return false;
+    }
 
     let op = document.querySelector('#operator').value;
 
     let calculate;
 
-    if(op == "add") {
+    if (op == "add") {
         calculate = a + b;
     } else if (op == "minus") {
         calculate = a - b;
@@ -19,5 +27,7 @@ function calc () {
         calculate = a * b;
     }
 
-    document.querySelector('#result').innerHTML = 'Answer ' + calculate;
+    const res = document.querySelector('#result');
+    res.innerHTML = 'Answer: ' + calculate
+    res.style = 'display = show'
 }
